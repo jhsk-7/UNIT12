62262986fb77e76f6a427fe6cc462361ae4067d9
Jordan <jhimpinfo@gmail.com>
2025-03-02 13:16:16 -0500

    Unit 12.8 ex 12

---
+++ ex12-8-12.js
@@ -0,0 +1,18 @@
+// Let variable will allow you to change the destination.
+let destination = "Ancient Egypt";
+console.log(destination)
+destination = "Medieval Europe";
+console.log(destination)
+
+// If line #10 is uncommented, the console will display an error
+// because a const variable cannot be changed.
+const travelDate = "2024-03-15";
+//travelDate = "2025-03-15"
+console.log(travelDate)
+
+// Line 16 will tell user that the variable timeMachineModel
+// is undefined. It will not throw an error message.
+// Line 18 will display the value of timeMachine Model "T-800"
+console.log(timeMachineModel)
+var timeMachineModel = "T-800"
+console.log(timeMachineModel)
